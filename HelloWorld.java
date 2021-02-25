0922879395856b1b8dd45b26bbeadd666bca4b0e
Mantu <sunarmahendra88@gmail.com>
2021-02-25 08:17:03 -0600

    New File Added

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+package san.hello;
+
+public class HelloWorld {
+
+	public static  void main(String[] args) {
+		String name="max well";
+		System.out.println("Hello"+""+name+"hey bro");
+		System.out.println("From nepal");
+	}
+}
